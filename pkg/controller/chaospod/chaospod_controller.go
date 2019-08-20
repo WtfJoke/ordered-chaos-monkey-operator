8732e9a6aca7600f2f7a92fe0f90e0420b2bfa25
Manuel <manuel.wessner@codecentric.de>
2019-08-20 16:12:24 +0200

    :bug: Dont requeue owner if changing pod

--- pkg/controller/chaospod/chaospod_controller.go
+++ pkg/controller/chaospod/chaospod_controller.go
@@ -55,12 +55,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner ChaosPod
-	err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &chaosv1alpha1.ChaosPod{},
-	})
 	if err != nil {
 		return err
 	}
